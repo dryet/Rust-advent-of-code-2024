a2d92ac2b414b027c93fbdb60c12496257a77251
Petr <opravilpeta@seznam.cz>
2025-04-23 22:36:05 +0200

    Finish day 2 (thanks Dome)

--- day2/part2/src/main.rs
+++ day2/part2/src/main.rs
@@ -12,69 +12,36 @@ fn main() {
         if row_string.is_empty() {
             break;
         }
-        let mut row: Vec<_> = row_string
+        let row: Vec<_> = row_string
             .split(' ')
             .map(|x| x.parse::<i32>().expect("Failed to parse to int"))
             .collect();
 
-        let mut incremented = false;
         if row
             .windows(2)
             .all(|a| a[0] < a[1] && a[0].abs_diff(a[1]) <= 3)
+            || row
+                .windows(2)
+                .all(|a| a[0] > a[1] && a[0].abs_diff(a[1]) <= 3)
         {
             result += 1;
-            incremented = true;
         } else {
-            let position = row
-                .windows(2)
-                .position(|a| a[0] > a[1] || a[0] == a[1] || a[0].abs_diff(a[1]) > 3);
+            for i in 0..row.iter().count() {
+                let mut row_cpy = row.clone();
+                row_cpy.remove(i);
 
-            if position != None {
-                let index = position.unwrap();
-                let mut temp_row = row.clone();
-                temp_row.remove(index);
-
-                if temp_row
+                if row_cpy
                     .windows(2)
                     .all(|a| a[0] < a[1] && a[0].abs_diff(a[1]) <= 3)
-                    || temp_row
+                    || row_cpy
                         .windows(2)
                         .all(|a| a[0] > a[1] && a[0].abs_diff(a[1]) <= 3)
                 {
                     result += 1;
-                    incremented = true;
+                    break;
                 }
             }
         }
-
-        if !incremented {
-            if row
-                .windows(2)
-                .all(|a| a[0] > a[1] && a[0].abs_diff(a[1]) <= 3)
-            {
-                result += 1;
-            } else {
-                let position = row
-                    .windows(2)
-                    .position(|a| a[0] < a[1] || a[0] == a[1] || a[0].abs_diff(a[1]) > 3);
-
-                if position != None {
-                    let index = position.unwrap();
-                    row.remove(index);
-
-                    if row
-                        .windows(2)
-                        .all(|a| a[0] < a[1] && a[0].abs_diff(a[1]) <= 3)
-                        || row
-                            .windows(2)
-                            .all(|a| a[0] > a[1] && a[0].abs_diff(a[1]) <= 3)
-                    {
-                        result += 1;
-                    }
-                }
-            }
-        }
-
-        println!("{result}");
     }
+    println!("{result}");
 }
