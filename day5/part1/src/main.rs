7ef3a9667fb639e9eed361ec345dcfcc435cf597
dryet <opravilpeta@seznam.cz>
2025-05-19 00:04:09 +0200

    Start day 5

---
+++ day5/part1/src/main.rs
@@ -0,0 +1,13 @@
+use std::env;
+use std::fs;
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let file_path = &args[1];
+    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+
+    let (mut rules, mut updates) = contents.split_once("\n\n").unwrap();
+    let update_pairs;
+    if (update_pairs.iter.all(|&x| rules.contains(x))) {
+        
+    }
+}
